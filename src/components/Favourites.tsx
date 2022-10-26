0e32f4de6b2a4a78e2f9a93bd7d2e04f3d1c9652
Juan Bustamante <bjuan210302@gmail.com>
2022-10-25 19:13:44 -0500

    Update Favourites.tsx

--- src/components/Favourites.tsx
+++ src/components/Favourites.tsx
@@ -1,65 +1,24 @@
-import { Grid, Button } from '@mantine/core';
-import { showNotification } from "@mantine/notifications";
-import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { Grid } from '@mantine/core';
+import { useContext } from "react";
 import { userContext } from "../main";
-import { saveFavourite } from "../utils/firebaseUtils";
 import PokemonCard from '../components/listPokemon/components/PokemonCard';
 
-type Item = {
-  name: string,
-  url: string,
-}
-
-const API = 'https://pokeapi.co/api/v2/pokemon/';
-
 function Favourites() {
-  const { uid, favourites, setFavourites } = useContext(userContext);
-  const [currentPage, setCurrentPage] = useState(0);
-  const [itemsCurrentPage, setItemsCurrentPage] = useState<Array<Item>>([])
-
-  useEffect(() => {
-    axios.get(API).then((res) => {
-      setItemsCurrentPage(res.data.results)
-      console.log('melo'+res.data.results)
-    });
-    console.log(favourites);
-  }, [])
-
-  const savePokemon = async () => {
-    try {
-      const newFavs = await saveFavourite(uid, itemsCurrentPage[currentPage])
-      setFavourites(newFavs);
-      showNotification({
-        message: 'Pokemon saved',
-        color: 'green',
-      })
-      setCurrentPage((current) => current + 1)
-    } catch (e: any) {
-      showNotification({
-        message: e.message,
-        color: 'red',
-      })
-    }
-  }
+  const { favourites } = useContext(userContext);
 
   const renderPokemon = () => {
-      return Object.keys(favourites).map((favKey => {
-        return( <Grid.Col span={3}>
-          <PokemonCard name={favourites[favKey].name} url={favourites[favKey].url} key={favourites[favKey].name}
-            isFavCard></PokemonCard>
-        </Grid.Col>)
-      }))
-  } 
+    return Object.keys(favourites).map((favKey => {
+      return (<Grid.Col span={3}>
+        <PokemonCard name={favourites[favKey].name} url={favourites[favKey].url} key={favourites[favKey].name}
+          isFavCard></PokemonCard>
+      </Grid.Col>)
+    }))
+  }
 
   return (
-    <>
-      <Grid>
-        {renderPokemon()}
-      </Grid>
-
-      <Button onClick={savePokemon}>Save</Button>
-    </>
+    <Grid>
+      {renderPokemon()}
+    </Grid>
   );
 }
 
